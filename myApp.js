381f0007520df322056f9468ffeebdf69cc78b0e
Bhornpat <thebowiiz2543@gmail.com>
2025-06-03 15:29:19 +0700

    edit

--- myApp.js
+++ myApp.js
@@ -1,16 +1,15 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
-
-module.exports = app;
-
-
-
+// Requirement #1: log to console
 console.log("Hello World");
 
+// Requirement #2: create route for "/"
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
 
-
-
+module.exports = app;
 
 
 
